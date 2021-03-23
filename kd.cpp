a1befaf710f362596daaa7aeed214452c74e612b
Rodion0 <jtham3000@gmail.com>
2021-03-23 19:29:16 -0400

    03/23/21 002

--- kd.cpp
+++ kd.cpp
@@ -10,17 +10,23 @@
 #include <iostream>
 
 //Node Class
-//Wrap all things into k dimensional values
-
+//Wrap all things into k dimensional pairs
 class node
 {
 private:
-    int x;
-    int y;
+    std::vector<int> values;
+    node *rightChild;
+    node *leftChild;
 
 public:
+    friend class kdTree;
     node(int value);
 };
+
+node::node(int value)
+{
+    values.push_back(value);
+}
 
 //KD Tree Class
 
@@ -34,13 +40,44 @@ public:
     //insert method
     void buildTree(std::vector<node> listOfPoints);
     //Search method
-    int search(int probe);
+    int search(node *current, int probe);
     //Find Best Axis
+    int findDimension(std::vector<node> listOfPoints);
+    //Find the Pivot Value
+    int findPivot(std::vector<node> listOfPoints, int index);
 };
+
+//Find Max in list of nodes
+int kdTree::findDimension(std::vector<node> listOfPoints)
+{
+    int dim;
+    //Loop through all nodes and insert maxes into maxes
+    //Find max element, set equal to iter
+    //return iter - listOfPoints.begin()
+    return dim;
+}
+
+int kdTree::findPivot(std::vector<node> listOfPoints, int index)
+{
+    int pivot;
+    //Put all values at given index into vector
+    //Find median value of vector using Lomuto's
+    //return pivot
+    return pivot;
+}
+
+void kdTree::buildTree(std::vector<node> listOfPoints)
+{
+    //Find best Dimension
+    //Find pivot
+    //Seperate based on pivot
+    //Recuse until bucket is filled
+}
 
 //Main
 int main(int argc, char const *argv[])
 {
+    //Parameters
 
     std::vector<node> keeper;
 
