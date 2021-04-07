81514a8b3fd3c266efd43a6ed384e494f06ac276
Rodion0 <jtham3000@gmail.com>
2021-04-06 21:25:03 -0400

    Finished Main

--- kd.cpp
+++ kd.cpp
@@ -42,7 +42,7 @@ void node::insert(int value)
     if (value > largest_number)
     {
         largest_number = value;
-        index_of_largest_number = values.size() - 1; 
+        index_of_largest_number = values.size() - 1;
     }
     values.push_back(value);
 }
@@ -53,27 +53,39 @@ class kdTree
 {
 private:
     std::vector<int> bucket;
+    node *root;
+
 public:
     kdTree();
+    //Get root
+    node *getRoot();
     //insert method
-    void buildTree(std::vector<node*> listOfPoints);
+    kdTree *buildTree(std::vector<node *> listOfPoints);
     //Search method
-    int search(node* current, int probe);
+    int search(node *current, std::vector<node *> listOfProbes);
     //Find Best Dimension
-    int findDimension(std::vector<node*> listOfPoints);
+    int findDimension(std::vector<node *> listOfPoints);
     //Find the Pivot Value using Lomuto's
-    int findPivot(std::vector<node*> listOfPoints, int dimension);
+    int findPivot(std::vector<node *> listOfPoints, int dimension);
+    //Fill the Buckets
 };
 
+//Get Root
+node *kdTree::getRoot()
+{
+    return root;
+}
+
 //Find Max in list of nodes
-int kdTree::findDimension(std::vector<node*> listOfPoints)
+int kdTree::findDimension(std::vector<node *> listOfPoints)
 {
     int dim, highest_number = INT_MIN;
-    //Loop through all nodes 
+    //Loop through all nodes
     for (int i = 0; i < listOfPoints.size(); i++)
     //Keep track of highest number and record its index in values array
     {
-        if(listOfPoints[i]->index_of_largest_number > highest_number){
+        if (listOfPoints[i]->index_of_largest_number > highest_number)
+        {
             dim = listOfPoints[i]->index_of_largest_number;
         }
     }
@@ -81,7 +93,7 @@ int kdTree::findDimension(std::vector<node*> listOfPoints)
     return dim;
 }
 
-int kdTree::findPivot(std::vector<node*> listOfPoints, int dimension)
+int kdTree::findPivot(std::vector<node *> listOfPoints, int dimension)
 {
     int pivot;
     //Put all values at given dimension into vector
@@ -91,16 +103,23 @@ int kdTree::findPivot(std::vector<node*> listOfPoints, int dimension)
     return pivot;
 }
 
-void kdTree::buildTree(std::vector<node*> listOfPoints)
+kdTree *kdTree::buildTree(std::vector<node *> listOfPoints)
 {
+
     //Find best Dimension
+    int dimension = findDimension(listOfPoints);
     //Find pivot
+    int pivot = findPivot(listOfPoints, dimension);
     //Seperate based on pivot
+
     //Recuse until bucket is filled (size 10)
 }
 
-int kdTree::search(node *current, std::vector<node*>probes){
-    //Idk even know how I forgot this 
+int kdTree::search(node *current, std::vector<node *> probes)
+{
+    //For All probes in Probes vector
+    //Put into Tree
+    //List all numbers in bucket
 }
 
 //Main
@@ -108,9 +127,9 @@ int main(int argc, char const *argv[])
 {
     //Parameters
     int numOfDim = std::stoi(argv[1]), numOfData = std::stoi(argv[2]), numOfProbes = std::stoi(argv[3]), data;
-    std::vector<node*> holding_cells;
-    std::vector<node*> probes;
-    kdTree tree; 
+    std::vector<node *> holding_cells;
+    std::vector<node *> probes;
+    kdTree *tree;
 
     //Fill Up Nodes/Store in Holding
     //Use Nested For Loops
@@ -119,26 +138,26 @@ int main(int argc, char const *argv[])
         node *temp = new node();
         for (int i = 0; i < numOfDim; i++)
         {
-            std::cin << data;
+            std::cin >> data;
             temp->insert(data);
         }
         holding_cells.push_back(temp);
     }
-    
+
     //Build Trees with whats in holding
-    tree.buildTree(holding_cells);
+    tree = tree->buildTree(holding_cells);
     //Fill some nodes for Probing from input
     for (int i = 0; i < numOfProbes; i++)
     {
         node *temp = new node();
         for (int i = 0; i < numOfDim; i++)
         {
-            std::cin << data;
+            std::cin >> data;
             temp->insert(data);
         }
-        holding_cells.push_back(temp);
+        probes.push_back(temp);
     }
     //Do the Search
-    tree.search(probes);
+    tree->search(tree->getRoot(), probes);
     return 0;
 }
